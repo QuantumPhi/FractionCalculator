b64b5fa16bd52236700410ee54c8866896531f9b
Tarik Onalan <phi.quantum@gmail.com>
2013-12-07 23:47:03 -0800

    +Added BigIntFraction Class +Added Fraction Interface

---
+++ FractionCalculator/src/calculator/Fraction.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public interface Fraction {
+	Fraction add (Fraction other);
+	Fraction subtract(Fraction other);
+	Fraction multiply(Fraction other);
+	Fraction divide(Fraction other);
+}
