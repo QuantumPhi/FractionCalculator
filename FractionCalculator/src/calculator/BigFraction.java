4a2c22c5e2e21b91873c24fde685c62c271caa3e
Tarik Onalan <phi.quantum@gmail.com>
2013-12-09 23:08:21 -0800

    =BigFraction commit

---
+++ FractionCalculator/src/calculator/BigFraction.java
@@ -0,0 +1,108 @@
+package calculator;
+
+import java.math.BigInteger;
+
+public class BigFraction implements Fraction {
+	private BigInteger numerator;
+	private BigInteger denominator;
+
+	public BigInteger getNumerator() {
+		BigInteger returnNum = new BigInteger(String.valueOf(this.numerator));
+		return returnNum;
+	}
+	public BigInteger getDenominator() {
+		BigInteger returnDenom = new BigInteger(String.valueOf(this.denominator));
+		return returnDenom;
+	}
+
+	public BigFraction(boolean positive, BigInteger numerator, BigInteger denominator) {
+		this.numerator = positive ? numerator : numerator.negate();
+		this.denominator = denominator;
+	}
+	
+	public BigFraction(boolean positive, BigInteger wholePart, BigInteger numerator, BigInteger denominator) {
+		this.numerator = positive ? numerator.add(wholePart.multiply(denominator)) : numerator.add(wholePart.multiply(denominator)).negate();
+		this.denominator = denominator;
+	}
+
+	
+	public Fraction add(Fraction other) {
+		Fraction newFraction = null;
+		BigInteger totalNum = this.numerator.multiply(other.getDenominator()).add(other.getNumerator().multiply(this.denominator));
+		BigInteger totalDenom = this.denominator.multiply(other.getDenominator());
+		newFraction = new BigFraction(true, totalNum, totalDenom);
+		newFraction.simplify();
+		return newFraction;
+	}
+	
+	public Fraction subtract(Fraction other) {
+		Fraction newFraction = null;
+		BigInteger totalNum = this.numerator.multiply(other.getDenominator()).subtract(other.getNumerator().multiply(this.denominator));
+		BigInteger totalDenom = this.denominator.multiply(other.getDenominator());
+		newFraction = new BigFraction(true, totalNum, totalDenom);
+		newFraction.simplify();
+		return newFraction;
+	}
+	
+	public Fraction multiply(Fraction other) {
+		BigInteger totalNum = this.numerator.multiply(other.getNumerator());
+		BigInteger setDenom = this.denominator.multiply(other.getDenominator());
+		Fraction newFraction = new BigFraction(true, totalNum, setDenom);
+		newFraction.simplify();
+		return newFraction;
+	}
+	
+	public Fraction divide(Fraction other) {
+		BigInteger totalNum = this.numerator.multiply(other.getDenominator());
+		BigInteger setDenom = this.denominator.multiply(other.getNumerator());
+		Fraction newFraction = new BigFraction(true, totalNum, setDenom);
+		newFraction.simplify();
+		return newFraction;
+	}
+	
+	public boolean compare(Fraction other) {
+		BigInteger compareInteger = this.numerator.multiply(other.getDenominator()).max(other.getNumerator().multiply(this.denominator));
+		boolean compareVal = compareInteger == this.numerator.multiply(other.getDenominator()) ? true : false;
+		return compareVal;
+	}
+	
+	public void simplify() {
+		BigInteger factorNum = this.numerator.abs();
+		BigInteger factorDenom = this.denominator;
+		while(!factorNum.equals(BigInteger.ZERO)) {
+			BigInteger temp;
+			temp = factorNum;
+			factorNum = factorDenom.mod(factorNum);
+			factorDenom = temp;
+		}
+		this.numerator = this.numerator.divide(factorDenom);
+		this.denominator = this.denominator.divide(factorDenom);
+		if(!this.numerator.equals(this.numerator.abs()) && !this.denominator.equals(this.denominator.abs())) {
+			this.numerator = this.numerator.negate();
+			this.numerator = this.denominator.negate();
+		}
+		if(this.numerator.equals(this.numerator.abs()) && !this.denominator.equals(this.denominator.abs())) {
+			this.numerator = this.numerator.negate();
+			this.denominator = this.denominator.abs();
+		}
+	}
+	
+	public String toString() {
+		String returnString = "";
+		BigInteger wholePart = BigInteger.ZERO;
+		if(this.numerator.equals(this.numerator.abs().compareTo(this.denominator.abs()))) {
+			BigInteger setNum = this.numerator.mod(this.denominator);
+			wholePart = this.numerator.subtract(setNum).divide(this.denominator);
+			this.numerator = setNum;
+		}
+		if(numerator.equals(BigInteger.ZERO))
+			returnString += wholePart;
+		else {
+			returnString = wholePart.equals(BigInteger.ZERO) ? returnString : returnString + wholePart + "_";
+			returnString = wholePart.equals(BigInteger.ZERO) ? returnString + this.numerator + "/" : returnString + this.numerator.abs() + "/";
+			returnString += this.denominator;
+		}
+		
+		return returnString;
+	}
+}
