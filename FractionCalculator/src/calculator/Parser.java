8f474a1140c5ee7a365e5afc935b6f91b29d8cb8
QuantumPhi <phi.quantum@gmail.com>
2014-01-08 08:21:20 -0800

    A VERY SIMPLE UPDATE! <3
    =Simplified parser class

--- FractionCalculator/src/calculator/Parser.java
+++ FractionCalculator/src/calculator/Parser.java
@@ -5,48 +5,25 @@ import java.math.BigInteger;
 
 public class Parser {
 	public static Fraction parseFraction(String input, boolean fractionType) {
-		if(fractionType) {
-			int wholePart = 0;
-			int numerator = 0;
-			int denominator = 1;
-			if(!input.contains("/"))
-				wholePart = Integer.parseInt(input.substring(0, input.length()));
-			else if(input.contains("_") && input.contains("/")) {
-				wholePart = Integer.parseInt(input.substring(0, input.indexOf('_')));
-				numerator = Integer.parseInt(input.substring(input.indexOf('_') + 1, input.indexOf('/')));
-				numerator = wholePart < 0 && numerator > 0 ? -numerator : numerator;
-				numerator = input.charAt(0) == '-' && wholePart == 0 ? -numerator : numerator;
-				denominator = Integer.parseInt(input.substring(input.indexOf('/') + 1, input.length()));
-			}
-			else if(!input.contains("_") && input.contains("/")) {
-				numerator = Integer.parseInt(input.substring(0, input.indexOf('/')));
-				denominator = Integer.parseInt(input.substring(input.indexOf('/') + 1, input.length()));
-			}
-			Fraction newFraction = new IntFraction(true, wholePart, numerator, denominator);
-			newFraction.simplify();
-			return newFraction;
+		BigInteger wholePart = BigInteger.ZERO;
+		BigInteger numerator = BigInteger.ZERO;
+		BigInteger denominator = BigInteger.ONE;
+		if(!input.contains("/"))
+			wholePart =  new BigInteger(input.substring(0, input.length()));
+		else if(input.contains("_") && input.contains("/")) {
+			wholePart = new BigInteger(input.substring(0, input.indexOf('_')));
+			numerator = new BigInteger(input.substring(input.indexOf('_') + 1, input.indexOf('/')));
+			numerator = wholePart.compareTo(BigInteger.ZERO) < 0 && numerator.compareTo(BigInteger.ZERO) > 0 ? numerator.negate() : numerator;
+			numerator = input.charAt(0) == '-' && wholePart.equals(BigInteger.ZERO) ? numerator.negate() : numerator;
+			denominator = new BigInteger(input.substring(input.indexOf('/') + 1, input.length()));
 		}
-		else {
-			BigInteger wholePart = BigInteger.ZERO;
-			BigInteger numerator = BigInteger.ZERO;
-			BigInteger denominator = BigInteger.ONE;
-			if(!input.contains("/"))
-				wholePart = new BigInteger(input.substring(0, input.length()));
-			else if(input.contains("_") && input.contains("/")) {
-				wholePart = new BigInteger(input.substring(0, input.indexOf('_')));
-				numerator = new BigInteger(input.substring(input.indexOf('_') + 1, input.indexOf('/')));
-				numerator = !wholePart.equals(wholePart.abs()) && numerator.equals(numerator.abs()) ? numerator.negate() : numerator;
-				numerator = input.charAt(0) == '-' && wholePart.equals(BigInteger.ZERO) ? numerator.negate() : numerator;
-				denominator = new BigInteger(input.substring(input.indexOf('/') + 1, input.length()));
-			}
-			else if(!input.contains("_") && input.contains("/")) {
-				numerator = new BigInteger(input.substring(0, input.indexOf('/')));
-				denominator = new BigInteger(input.substring(input.indexOf('/') + 1, input.length()));
-			}
-			Fraction newFraction = new BigFraction(true, wholePart, numerator, denominator);
-			newFraction.simplify();
-			return newFraction;
+		else if(!input.contains("_") && input.contains("/")) {
+			numerator = new BigInteger(input.substring(0, input.indexOf('/')));
+			denominator = new BigInteger(input.substring(input.indexOf('/') + 1, input.length()));
 		}
+		Fraction newFraction = fractionType ? new IntFraction(true, wholePart.intValue(), numerator.intValue(), denominator.intValue()) : new BigFraction(true, wholePart, numerator, denominator);
+		newFraction.simplify();
+		return newFraction;
 	}
 	
 	public static Fraction parseExpression(List<Fraction> fracArray, List<String> opString) {
